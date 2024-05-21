ab1c5b4b8ea72df205131d5ce1bca4d25f12befd
muhamejanov11 <muhamejanov.n@gmail.com>
2024-05-21 21:14:08 +0500

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+let p = document.querySelector('p')
+
+let container = document.querySelector('.con')
+
+let bgPrompt = prompt('Напишите цвет заднего фона')
+
+let pPrompt = prompt('Напишите цвет текста')
+
+
+p.style.color = pPrompt
+container.style.backgroundColor = bgPrompt
